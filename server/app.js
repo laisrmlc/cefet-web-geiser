c93aa31e5876bf01b123252abd2f8ee28785fa3a
Aluno <aluno@decom-OptiPlex-780>
2016-05-12 12:23:59 -0300

    Geiser incompleto

--- server/app.js
+++ server/app.js
@@ -1,11 +1,42 @@
+var fs = require('fs');
 var express = require('express'),
     app = express();
+
+  var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server started://%s:%s', host, port);
+});
+
+app.use(express.static('client'));
+/*app.get('server', 
+	function(request, response){
+		response.render('index.js');		
+	});*/
+app.set('view engine', 'hbs');
+app.set('views', 'server/views');
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
+
 var db = {
+	
+
 };
+
+fs.readFile('server/data/jogadores.json',
+	function(data){
+		db = JSON.parse(data);
+	}
+);
+
+app.get('/', 
+	function(request, response){
+		response.render('index.hbs', db);
+
+	}
+);
 
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
