cf13f700ee2c5ed5885b50e087de9f6531d83ef5
laisrmlc <laisrmlc@gmail.com>
2016-05-12 22:43:02 -0300

    Geiser complete

--- server/app.js
+++ server/app.js
@@ -1,18 +1,15 @@
-var fs = require('fs');
+var fs = require('fs');  //import do fs - modulo do sistema pra acessar arquivos e diretórios
+var _ = require('underscore');
 var express = require('express'),
-    app = express();
+    app = express(); //modulo para criar servidor
 
-  var server = app.listen(3000, function () {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Server started://%s:%s', host, port);
 });
 
 app.use(express.static('client'));
-/*app.get('server', 
-	function(request, response){
-		response.render('index.js');		
-	});*/
 app.set('view engine', 'hbs');
 app.set('views', 'server/views');
 
@@ -20,22 +17,67 @@ app.set('views', 'server/views');
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
 
-var db = {
-	
-
-};
+var db = [];
 
 fs.readFile('server/data/jogadores.json',
-	function(data){
-		db = JSON.parse(data);
+	function(err, data){
+		db[0] = JSON.parse(data);
 	}
 );
 
-app.get('/', 
+fs.readFile('server/data/jogosPorJogador.json',
+	function(err, data){
+		db[1] = JSON.parse(data);
+	}
+);
+
+app.get('/',
 	function(request, response){
-		response.render('index.hbs', db);
+		response.render('index.hbs', db[0]);
 
 	}
+);
+
+app.get('/jogador/:numero_identificador/',
+  function(request, response){
+    var pag={};
+    var i,j;
+    var id = request.params.numero_identificador;
+
+    for(i=0 ; i<db[0].players.length ; i++){
+      if (db[0].players[i].steamid == id){
+        pag.player = db[0].players[i];
+      }
+    }
+   pag.infogames = db[1][id];
+   var cont=0;
+   var played=[];
+    for(j=0 ; j < pag.infogames.games.length ; j++ ){
+
+        if(pag.infogames.games[j].playtime_forever ==0){
+          cont++;
+        }
+        pag.infogames.games[j].playTimeHr = ((pag.infogames.games[j].playtime_forever)/60).toFixed(0);
+    }
+
+    var morePlayed = _.first(_.sortBy(pag.infogames.games, function(game){ //pega os 5 mais jogados
+        return -game.playtime_forever;
+      }
+    ),5);
+    var timetop=[];
+    for(i=0 ; i < 5 ; i++){
+      timetop[i] = (morePlayed[i].playtime_forever/60).toFixed(0);
+    }
+
+    pag.timeMostPlayed = timetop[0];
+    pag.timeTop = timetop;
+    pag.mostplayed = _.first(morePlayed);
+    pag.moreplayed = morePlayed;
+    pag.timePlayed = played;
+    pag.notPlayed = cont;
+
+    response.render('jogador.hbs', pag);
+  }
 );
 
 
